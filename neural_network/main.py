69297bccbbe6ad8efad9817ef9e83d9cbd490f7d
Valdemar Emil Otte Petersen <36330221+ValdemarOtte@users.noreply.github.com>
2023-10-17 10:16:25 +0200

    fix random points

--- neural_network/main.py
+++ neural_network/main.py
@@ -1,9 +1,10 @@
 import random
+from matplotlib import pyplot as plt
 
 
-def create_random_points(min_: int, max_: int, n: int) -> list[list[float]]:
+def create_random_points_blue(min_: int, max_: int, n: int) -> list[list[float]]:
     """
-    Create random points.
+    Create random blue points.
 
     Args:
     ----
@@ -23,7 +24,40 @@ def create_random_points(min_: int, max_: int, n: int) -> list[list[float]]:
     return points
 
 
+def create_random_points_red(min_: int, max_: int, n: int) -> list[list[float]]:
+    """
+    Create random red points.
+
+    Args:
+    ----
+        min_: Minimum for the value of the point
+        max_: Maxsimun for the value of the point
+        n: Number of points which need to be created
+
+    Returns:
+    -------
+        A list of random created points
+    """
+    points = []
+    for _ in range(n):
+        while True:
+            x = round(random.uniform(min_, max_), 2)  # noqa: S311
+            y = round(random.uniform(min_, max_), 2)  # noqa: S311
+            if x + y > 8:
+                break
+        points.append([x, y])
+    return points
+
+
 if __name__ == "__main__":
-    points_blue = create_random_points(0, 5, 20)
-    points_red = create_random_points(5, 20, 80)
+    points_blue = create_random_points_blue(0, 8, 20)
+    points_red = create_random_points_red(0, 20, 80)
     points = points_blue + points_red
+
+    for point in points_blue:
+        plt.plot(point[0], point[1], marker="o", markeredgecolor="blue", markerfacecolor="blue")
+    for point in points_red:
+        plt.plot(point[0], point[1], marker="o", markeredgecolor="red", markerfacecolor="red")
+    plt.xlim(0, 20)
+    plt.ylim(0, 20)
+    plt.show()
